6a77687ea5703540ae3eb0c89bdde52a8dec22c8
craciun <craci@DESKTOP-0VNQOH1>
2018-09-18 11:14:07 +0300

    First commit - efficiency improvements might go in the future

---
+++ Sandpiles/Sandpiles/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sandpiles
+{
+    public partial class Form1 : Form
+    {
+
+        public Graphics gfx;
+
+        public Sandpile sandpile;
+        public int width, height;
+
+        public const int amount = 100000;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gfx = pictureBox1.CreateGraphics();
+            width = pictureBox1.Width;
+            height = pictureBox1.Height;
+
+            sandpile = new Sandpile(width, height);
+            sandpile.Drop(amount);
+            
+            sandpile.Topple();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+            gfx.Clear(Color.Black);
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    switch (sandpile.Data()[i, j])
+                    {
+                        case 1:
+                            {
+                                gfx.DrawRectangle(new Pen(Color.White), i, j, 1, 1);
+                                break;
+                            }
+                        case 2:
+                            {
+                                gfx.DrawRectangle(new Pen(Color.Purple), i, j, 1, 1);
+                                break;
+                            }
+                        case 3:
+                            {
+                                gfx.DrawRectangle(new Pen(Color.Brown), i, j, 1, 1);
+                                break;
+                            }
+
+                    }
+                    
+                }
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public static void LogMatrix(int[,] m)
+        {
+
+        }
+    }
+}
---
+++ Sandpiles/Sandpiles/Sandpile.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandpiles
+{
+    public class Sandpile
+    {
+
+        private int[,] sandpile;
+
+        private int width, height;
+
+        public Sandpile(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+
+            sandpile = new int[height, width];
+        }
+        
+        public int[,] Data()
+        {
+            return sandpile;
+        }
+
+        public void Drop(int amount)
+        {
+            sandpile[height / 2, width / 2] = amount;
+        }
+
+        public int GetWidth()
+        {
+            return width;
+        }
+
+        public int GetHeight()
+        {
+            return height;
+        }
+
+        public void Topple()
+        {
+            bool finish = false;
+            while (!finish)
+            {
+                bool once = false;
+                for (int i = 0; i < height; i++)
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        if (sandpile[i, j] > 3)
+                        {
+                            if (j - 1 >= 0)
+                                sandpile[i, j - 1] += sandpile[i, j] / 4;
+                            if (j + 1 <= width)
+                                sandpile[i, j + 1] += sandpile[i, j] / 4;
+                            if (i - 1 >= 0)
+                                sandpile[i - 1, j] += sandpile[i, j] / 4;
+                            if (i + 1 <= height)
+                                sandpile[i + 1, j] += sandpile[i, j] / 4;
+
+                            sandpile[i, j] = sandpile[i, j] % 4;
+                            finish = false;
+                            once = true;
+                        }
+                        else if (!once)
+                        {
+                            finish = true;
+                        }
+
+
+                        if (sandpile[j, i] > 3)
+                        {
+                            if (i - 1 >= 0)
+                                sandpile[j, i - 1] += sandpile[j, i] / 4;
+                            if (i + 1 <= width)
+                                sandpile[j, i + 1] += sandpile[j, i] / 4;
+                            if (j - 1 >= 0)
+                                sandpile[j - 1, i] += sandpile[j, i] / 4;
+                            if (j + 1 <= height)
+                                sandpile[j + 1, i] += sandpile[j, i] / 4;
+
+                            sandpile[j, i] = sandpile[j, i] % 4;
+                            finish = false;
+                            once = true;
+                        }
+                        else if (!once)
+                        {
+                            finish = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
